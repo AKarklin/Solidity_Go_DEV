3005587276130a621edc7bd97ae74f820e386545
AKarklin <akarklin@mail.ru>
2022-05-23 19:36:13 +0300

    Update build.cmd

--- projects/Test01/build.cmd
+++ projects/Test01/build.cmd
@@ -1,3 +1,5 @@
-docker run -v c:/Klins/Ethereum/Solidity_Go_DEV:/root ethereum/solc:stable --optimize --abi /root/projects/Test01/MySmartContract.sol -o /root/projects/Test01/build
-docker run -v c:/Klins/Ethereum/Solidity_Go_DEV:/root ethereum/solc:stable --optimize --bin /root/projects/Test01/MySmartContract.sol -o /root/projects/Test01/build
+docker run -v c:/Klins/Ethereum/Solidity_Go_DEV:/root --name solc_abi ethereum/solc:stable --optimize --abi /root/projects/Test01/MySmartContract.sol -o /root/projects/Test01/build
+docker rm solc_abi
+docker run -v c:/Klins/Ethereum/Solidity_Go_DEV:/root --name solc_bin ethereum/solc:stable --optimize --bin /root/projects/Test01/MySmartContract.sol -o /root/projects/Test01/build
+docker rm solc_bin
 ..\..\abigen\abigen.exe --abi=build/MySmartContract.abi --bin=build/MySmartContract.bin --pkg=api --out=../../contracts_go_api/Test01/api/MySmartContract.go
